8181e585996caf960651eb5766de9664c00982d2
henzhero <189972177+henzhero@users.noreply.github.com>
2025-02-05 11:30:10 +0800

    changed syntax for adding class and moved styling to css file

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ const mainContainer = document.querySelector("#container");
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const square = document.createElement("div");
-        square.className = "square";
-        square.style.height = "30px";
-        square.style.width = "30px";
-        square.style.backgroundColor = "lightblue";
-        square.style.border = "1px solid black"
+        square.classList.add("square");
 
         mainContainer.appendChild(square);
     }
