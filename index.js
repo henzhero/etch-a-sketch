6b4f497bdaeee6bbde3f86024e79798f33ba0160
henzhero <189972177+henzhero@users.noreply.github.com>
2025-02-05 11:44:51 +0800

    added a class for changing color. added event delegation on mouse hover

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const mainContainer = document.querySelector("#container");
+const container = document.querySelector("#container");
 
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const square = document.createElement("div");
         square.classList.add("square");
 
-        mainContainer.appendChild(square);
+        // square.addEventListener("mouseover", (e) => {
+        //     square.style.backgroundColor = "pink";
+        // })
+
+        container.appendChild(square);
     }
 }
+
+container.addEventListener("mouseover", (e) => {
+    if (e.target.classList.contains("square")) {
+        e.target.classList.toggle("change");
+    }
+})
